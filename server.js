88568c961c8215a9f0a276fd367a98ebec58f006
rzhasibuan <rzhasibuan@gmail.com>
2021-03-30 15:04:41 +0700

    add dynamic GET route

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 //     console.log("server is running");
 // })
 
+// Get JSON data
 const mockUserData = [
-    {name: 'Reza'},
+    {name: 'reza'},
     {name: 'hasibuan'}
 ]
 
@@ -18,7 +19,19 @@ app.get('/users',function(req,res){
     })
 })
 
+// Use Variables in URL #5
+
+app.get('/users/:id',function(req,res){
+    console.log(req.params.id)
+    res.json({
+        success : true,
+        message : 'got one user',
+        user : req.params.id  // call data from parameter
+    });
+});
+
 app.listen(8000,function(){
     console.log("server is listening")
 })
+
 
