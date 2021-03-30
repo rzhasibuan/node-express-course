3aabd052705d5da26f3ea050621e61dcdc502a63
rzhasibuan <rzhasibuan@gmail.com>
2021-03-30 22:18:38 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// add body-parse library
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 // app.listen(8000,function(){
 //     console.log("server is running");
 // })
@@ -28,6 +32,30 @@ app.get('/users/:id',function(req,res){
         message : 'got one user',
         user : req.params.id  // call data from parameter
     });
+});
+
+// login with body parser 
+app.post('/login',function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "reza";
+    const mockPassword = "password";
+
+    if(username===mockUsername && password===mockPassword){
+        res.json({
+            success: true,
+            message:'password and username match',
+            token: 'encrypted token goes here'
+        });
+
+    }else{
+        res.json({
+            success:false,
+            message:'password and username not match'
+        })
+    }
+
 });
 
 app.listen(8000,function(){
